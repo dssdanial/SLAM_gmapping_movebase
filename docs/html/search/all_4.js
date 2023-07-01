b17fdf6c0ca27feac068ebe838ede1cb14dd445f
Danial Sabzevari <32397445+dssdanial@users.noreply.github.com>
2023-07-01 17:01:40 +0200

    Delete docs directory

--- docs/html/search/all_4.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['get_5fcoordinates_7',['get_coordinates',['../xy__position_8cpp.html#a979c1eec3a680dc9742ca8f3a498bf5f',1,'xy_position.cpp']]],
-  ['get_5finput_8',['get_input',['../_manual__drive_8cpp.html#a7f7d866eccdabe51bb16818a792618b1',1,'get_input():&#160;Manual_drive.cpp'],['../_manual___obs_av_8cpp.html#a7f7d866eccdabe51bb16818a792618b1',1,'get_input():&#160;Manual_ObsAv.cpp']]],
-  ['getch_9',['getch',['../_manual__drive_8cpp.html#af5978fab9fa6dd4ced1c3a8ab1251f7b',1,'getch(void):&#160;Manual_drive.cpp'],['../_manual___obs_av_8cpp.html#af5978fab9fa6dd4ced1c3a8ab1251f7b',1,'getch(void):&#160;Manual_ObsAv.cpp']]],
-  ['goal_10',['goal',['../xy__position_8cpp.html#a277a26d89f87198b128da14ee46d39a7',1,'xy_position.cpp']]]
-];
--- docs/html/search/variables_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['y_61',['y',['../xy__position_8cpp.html#aa4f0d3eebc3c443f9be81bf48561a217',1,'xy_position.cpp']]]
-];
